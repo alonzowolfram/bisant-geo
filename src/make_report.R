b08adf80386cc50e74a88c5dc12abac01112f8c0
Lon Fong <alonzo.fong@gmail.com>
2025-01-31 15:33:47 -0600

    Switching from PowerPoint to RMD reports

---
+++ src/make_report.R
@@ -0,0 +1,49 @@
+#!/usr/bin/env Rscript
+# https://training.nextflow.io/advanced/structure/#bin
+
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#
+# Setup -----------------------------------------
+#
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+# Source the setup.R file.
+# Note that we cannot use source() directly in Nextflow; see https://stackoverflow.com/questions/76067626/how-to-call-a-function-present-in-a-different-r-script-from-an-r-executable-usin
+# and https://community.seqera.io/t/source-another-r-script-in-the-bin-directory/1059
+# So we will have to use the workaround suggested above if the workflow system is Nextflow.
+cl_args <- commandArgs(TRUE)
+workflow_system <- cl_args[2]
+if(workflow_system=="Nextflow") {
+  path <- Sys.getenv("PATH") |> strsplit(":")
+  bin_path <- tail(path[[1]], n=1)
+  source(file.path(bin_path, "setup.R"))
+} else {
+  bin_path <- ""
+  source("src/setup.R") # I guess the path it sources from is the current working directory, not the path the R script lives in.
+}
+
+# Load the RDS objects with the necessary data (which is just the latest module completed). 
+rds_path <- cl_args[5]
+latest_module <- readRDS(rds_path)
+
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#
+# Make the report -----------------------------------------
+#
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+message(paste0("Rendering HTML report using the template found at ", rmd_template_file, "."))
+# saveRDS(iris, paste0(output_dir_pubs, "test.rds"))
+# Set the paths.
+pkc_summary_path <- paste0(output_dir_rdata, "pkc_summary_table.rds")
+qc_segments_summary_path <- paste0(output_dir_rdata, "qc-segments_summary_table.rds")
+ntc_summary_path <- paste0(output_dir_rdata, "ntc_summary_table.rds")
+plot_list_qc_segments_path <- paste0(output_dir_rdata, "qc-segments_plot_list.rds")
+# Render.
+rmarkdown::render(rmd_template_file,
+                  output_file=paste0(output_dir_pubs, "report.html"),
+                  params=list(qc_segments_summary_file=qc_segments_summary_path,
+                              ntc_summary_file=ntc_summary_path,
+                              pkc_summary_file=pkc_summary_path,
+                              plot_list_qc_segments_file=plot_list_qc_segments_path
+                              )
+                  )
