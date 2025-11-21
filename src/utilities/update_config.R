5b3daac5545cc639367937fba8c1a1c5be7e86a1
Lon Fong <alonzo.fong@gmail.com>
2025-11-21 11:32:02 -0600

    Working version of update_config.R

--- src/utilities/update_config.R
+++ src/utilities/update_config.R
@@ -7,22 +7,15 @@
 #
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-# Source the setup.R file.
-# Note that we cannot use source() directly in Nextflow; see https://stackoverflow.com/questions/76067626/how-to-call-a-function-present-in-a-different-r-script-from-an-r-executable-usin
-# and https://community.seqera.io/t/source-another-r-script-in-the-bin-directory/1059
-# So we will have to use the workaround suggested above if the workflow system is Nextflow.
-cl_args <- commandArgs(TRUE)
-workflow_system <- cl_args[2]
-if(workflow_system=="Nextflow") {
-  path <- Sys.getenv("PATH") |> strsplit(":")
-  bin_path <- tail(path[[1]], n=1)
-  source(file.path(bin_path, "setup.R"))
-} else {
-  bin_path <- ""
-  source("src/pipeline/setup.R") # I guess the path it sources from is the current working directory, not the path the R script lives in.
-}
-
+# Libraries
+library(tidyr)
 library(yaml)
+library(stringr)
+
+# Read in the variables from the arguments
+cl_args <- commandArgs(TRUE)
+master_yaml <- cl_args[1] # The main template `config.yaml` file
+project_yaml_list <- cl_args[2] %>% str_split(",") %>% unlist # A vector of configuration YAML files to be updated to match `master_yaml`, separated by commas
 
 # Functions
 trimTrailingWhitespace <- function(x) {
@@ -40,39 +33,39 @@ emptyOrWhitespace <- function(x) {
 }
 
 mergeConfig <- function(master_file, project_file) {
-  # Read config files as raw text.
+  # Read config files as raw text
   master_lines <- readLines(master_file, warn = FALSE) %>% sapply(trimTrailingWhitespace)
   project_lines <- if (file.exists(project_file)) readLines(project_file, warn = FALSE) %>% sapply(trimTrailingWhitespace) else NULL
   project_lines_non_comment <- project_lines %>% .[!startsWithHash(.)]
   
-  # Process project file line by line.
+  # Process project file line by line
   updated_lines <- c()
   for (line in master_lines) {
-    # Check if it's a comment or empty/whitespace line. 
+    # Check if it's a comment or empty/whitespace line
     if(startsWithHash(line) || emptyOrWhitespace(line)) {
       updated_lines <- c(updated_lines, line)
     } else {
-      # Check if the variable already exists in the copy (project_file).
-      # First extract the key only, not the value.
+      # Check if the variable already exists in the copy (project_file)
+      # First extract the key only, not the value
       key <- line %>% regexPipes::gsub(":.+$", "")
-      # Now look for this key in project_lines.
-      # Make sure to check only the non-comment lines. 
+      # Now look for this key in project_lines
+      # Make sure to check only the non-comment lines
       index <- base::grep(key, project_lines_non_comment)
       
       if(length(index) < 1) {
-        # It doesn't exist. 
-        # So add the line from the master file to `updated_lines`.
+        # It doesn't exist
+        # So add the line from the master file to `updated_lines`
         updated_lines <- c(updated_lines, line)
       } else {
-        # It does.
-        # So add the line from the project file to `updated_lines`.
+        # It does
+        # So add the line from the project file to `updated_lines`
         updated_lines <- c(updated_lines, project_lines_non_comment[index[1]])
       }
       
     }
   }
   
-  # Write the updated file.
+  # Write the updated file
   writeLines(updated_lines, project_file)
 }
 
@@ -82,10 +75,7 @@ mergeConfig <- function(master_file, project_file) {
 #
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-# Example usage
-master_yaml <- "~/Library/CloudStorage/OneDrive-InsideMDAnderson/PRIME-TR/projects/DSP-P001_bisant-geo/config.yaml"
-project_yaml_list <- c("~/Library/CloudStorage/OneDrive-InsideMDAnderson/PRIME-TR/projects/DSP-P001_bisant-geo/profiles/Lon/DSP-044/DSP-044_RNA_config_complete.yaml")
-
+# Loop over each project YAML and update to match the template
 for (project_yaml in project_yaml_list) {
   mergeConfig(master_yaml, project_yaml)
 }
